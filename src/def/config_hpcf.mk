d3fb5349eb96153eec49a3f0eacb84731fdf2dc5
Geoffroy Lesur <gl293@bindloe02.moab.cluster>
2009-04-08 09:32:13 +0100

    Fix to hpcf config file

--- src/def/config_hpcf.mk
+++ src/def/config_hpcf.mk
@@ -1,6 +1,6 @@
 OPENMP=no
 MPI=yes
-FFTW3_MPI=yes
+FFTW3_MPI=no
 DEBUG=no
 
 CLUSTER="HPCF Cambridge"
@@ -13,6 +13,6 @@ FFTPATH=-L/home/gl293/usr/lib
 CFLAGS=-O3 -I/home/gl293/usr/include
 OPENMP_FLAG=-openmp
 ifeq ($(DEBUG),yes)
-	CFLAGS=-g -DDEBUG
+	CFLAGS=-g -DDEBUG -I/home/gl293/usr/include
 	LDFLAGS+=-g
 endif
