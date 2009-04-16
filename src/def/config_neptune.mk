6771cb6f45e659b5895f6cc0de03b919dd4a4630
Geoffroy Lesur <gl293@neptune.damtp.cam.ac.uk>
2009-04-16 16:33:03 +0100

    new config for neptune

--- src/def/config_neptune.mk
+++ src/def/config_neptune.mk
@@ -6,7 +6,7 @@ DEBUG=no
 CLUSTER="SE Linux x86 (64 bits)"
 CC=icc
 FFTPATH=-L/home/raid/chaos/gl293/usr/lib
-CFLAGS=-O3 -xhost
+CFLAGS=-O3 -xHOST -ipo -no-prec-div -static
 OPENMP_FLAG=-openmp
 ifeq ($(DEBUG),yes)
 	CFLAGS=-g -DDEBUG
