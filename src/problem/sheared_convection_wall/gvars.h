6c5d31ab257e204be7e2ecdd41888e15e2699169
Geoffroy Lesur <gl293@neptune.damtp.cam.ac.uk>
2009-07-10 16:39:41 +0100

    added the symmetrize bit

--- src/problem/sheared_convection_wall/gvars.h
+++ src/problem/sheared_convection_wall/gvars.h
@@ -19,9 +19,9 @@
 #ifndef _GVARS_
 #define _GVARS_
 
-#define		NX				64			/**< X Dimension in real space. Must be multiples of NPROC when using MPI.*/
-#define		NY				64			/**< Y Dimension in real space. Must be multiples of NPROC when using MPI.*/
-#define		NZ				128			/**< Z Dimension in real space. */
+#define		NX				256			/**< X Dimension in real space. Must be multiples of NPROC when using MPI.*/
+#define		NY				256			/**< Y Dimension in real space. Must be multiples of NPROC when using MPI.*/
+#define		NZ				256			/**< Z Dimension in real space. */
 
 //#define		MHD						/**< Uncomment to activate MHD*/
 
